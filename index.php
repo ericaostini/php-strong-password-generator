28c6bdca54879290e858bd37b61aef16b62991c9
ericaostini <ericaostini14@gmail.com>
2023-11-30 14:13:30 +0100

    start commit and file README

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include __DIR__ . "/partials/header.php";
+?>
+
+<main class="container">
+    password generator
+</main>
+
+<?php
+include __DIR__ . "/partials/footer.php";
+?>
---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="container">
+    <h4>Made with love by Erica</h4>
+</footer>
+</body>
+
+</html>
